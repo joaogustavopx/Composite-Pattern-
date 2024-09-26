a234934f1e8146c13333984dd288ffccdf589f51
Alexandre Silveira de Paula <168464775+alexandresilveiradepaula@users.noreply.github.com>
2024-09-26 17:16:23 -0300

    teste

--- TCriteria.class.php
+++ TCriteria.class.php
@@ -1,105 +1,86 @@
-<?php 
-  /*
-  * Classe TCriteria
-  * Esta classe prove uma interface utilizada para definição de critérios
-  */
+<?php
+/*
+* Classe TCriteria
+* Esta classe provê uma interface para definição de critérios
+*/
 
-  class TCriteria extends TExpression
-  {
-    private $expressions; // armazena a lista de instruções
-    private $operators; // armazena a lista de operadores
-    private $properties; // propriedades do critério
+class TCriteria extends TExpression
+{
+    private $expressions; //armazena a lista de instruções
+    private $operators; //armazena a lista de operadores
+    private $properties; //propriedades do critério
 
-    // Metodo Construtor
-    function __construct()
-    { 
-      $this->expressions = array();
-      $this->operators = array();
+    //método construtor
+    function __construct(){
+        $this->expressions = array();
+        $this->operators = array();
     }
-    
     /*
-    * Metodo Add
-    * adiciona uma expressão ao criterio
-    * @param $expressions = expressão (objeto TExpresssion)
-    * @param $operator = operador lógico de comparação
+    *método add
+    *adiciona uma expreção ao critério
+    *@param $expressions = expressão (objeto TExpression)
+    *@param $operator = operador lógico de comparação
     */
-
     public function add(TExpression $expression, $operator = self::AND_OPERATOR)
     {
-      // na primeira vez, não precisamos de operador lógico para concatenar
-
-      if(empty($this->expressions))
-      {
-        $operator = NULL;
-      
-      }
-
-      // agrega o resultado da expressão a lista de expressões 
-      $this->expressions[] = $expression;
-      $this->operators[] = $operator;
-    
+        //na primeira vez, não precisamos de operador lógico para concatenar
+        if(empty($this->expressions))
+        {
+            $operator = NULL;
+        }
+        //agrega o resultado da expressão a lista de expressões
+        $this->expressions[] = $expression;
+        $this->operators[] = $operator;
     }
-
-    /* Método dump()  
-    * retorna a expressão final 
+    /*método dump
+    *retorna a expressão final
     */
 
     public function dump()
     {
-      // concatena a lista de expressoes
-      if(is_array($this->expressions))
-      {
-        if(count($this->expressions)>0)
-          {
-            $result = '';
-            foreach($this->expressions as  $i=> $expression)
-             {
-                $operator = $this->operators[$i];
-                // concatena o operador com a respectiva expressão
-                $result .= $operator. $expression->dump().' ';
-              }
+        //concatena a lista de expressões
 
-              $result = trim($result);
-              return "({$result})";
+        if(count($this->expressions) > 0)
+        {
+        $result = '';
+        foreach($this->expressions as $i => $expression)
+          {
+            $operator = $this->operators[$i];
+            //concatena o operador com a respectiva expressão
+            $result .= $operator . $expression->dump().' ';
+            }
+            $result = trim($result);
+            return "({$result})";
+        }
+
+    }
+    /* método SetProperty
+    *define o valor de uma propriedade
+    *@param = $property = propriedade
+    *@param = $value = valor 
+*/
+public function setProperty($property, $value)
+{
+    if(isset($value))
+    {
+        $this->properties[$property] = $value;
+    }
+    else
+    {
+        $this->properties[$property] = NULL;
+    }
+   }
+   /* método getproperty()
+   *retorna o valor da propriedade 
+   *@param $property = propriedade
+   */
+  public function getProperty($property)
+  {
+    if(isset($this->properties[$property]))
+    {
+    return $this->properties[$property];
     }
   }
 }
 
-  /* Metodo SetPropriety()
-  * define o valor de uma propriedade
-  * @param $property = propriedade
-  * @param $value = valor
-  */
-  public function setProperty($property,$value)
-    {
-      if(isset($value))
-      {
-        $this->properties[$property] = $value;
-      }
-      else
-      {
-        $this->properties[$property] = NULL;
-      }
-    }
-
-  /* Metodo getProperty()
-  * retorna o valor da propriedade
-  * @param $property = propriedade
-  */
-
-    public function getProperty($property)
-    {
-      if(isset($this->properties[$property]))
-      {
-            return $this->properties[$property];
-      }
-    }
-  }
-    
-      ?>
-
-
-
-
-      
-
+?>
--- criteria.php
+++ criteria.php
@@ -1,78 +1,87 @@
 <?php
-// carrega as classes necessárias
+ //carrega as classes necessárias 
 
-include_once 'TExpression.class.php';
-include_once 'TCriteria.class.php';
-include_once 'TFilter.class.php';
+ include_once 'TExpressions.class.php';
+ include_once 'TCriteria.class.php';
+ include_once 'TFilter.class.php';
 
-// aqui vemos um exemplo do critério utilizando o operador OR
-// a idade deve ser menor que 16 anos e maior que 60 anos
+ //aqui vemos um exemplo do critério utilizando o operador OR
+ // a idade deve ser menor que 16 anos e maior que 60 anos
 
 $criteria = new TCriteria;
-$criteria->add(new TFilter('idade','<',16),TExpression::OR_OPERATOR);
-$criteria->add(new TFilter('idade','>',60),TExpression::OR_OPERATOR);
+$criteria ->add(new TFilter('idade','<', 16), TExpression::OR_OPERATOR);
+$criteria ->add(new TFilter('idade','>',60), TExpression::OR_OPERATOR);
+
 echo $criteria->dump();
 echo "<br>\n";
 
-// aqui vemos um exemplo co critério utilizando o operador lógico AND
-//juntamente com os operadores de conjunto IN(dentro do conjunto) e 
-// NOT IN(fora dos conjunto)
-// a idade deve estar dentro do conjunto (24,25,26) e deve estar fora do conjunto (10)
+//aqui vemos um exemplo do critério utilizando o operador logico AND
+//juntamente com os operadores de conjunto IN(dentro do conjunto) e
+//NOT IN(fora dos conjuntos)
+//a idade deve estar dentro do conjunto (24, 25, 26) e deve estar fora do conjunto (10)
 
 $criteria = new TCriteria;
-$criteria->add(new TFilter("idade","IN", array(24,25,26)));
-$criteria->add(new TFilter("idade","NOT IN", array(10)));
+$criteria ->add(new TFilter("idade","IN",array(24, 25, 26)));
+$criteria ->add(new TFilter("idade","NOT IN",array(10)));
+
 echo $criteria->dump();
 echo "<br>\n";
 
-// aqui vemos um exemplo de critério utilizando o operador de comparação LIKE
-// o nome deve iniciar por "pedro" ou deve iniciar por "maria'
+//aqui vemos um exemplo de critério utilizando o operador de comparação
+//o nome deve iniciar por "pedro" ou deve iniciar por "maria"
+
 $criteria = new TCriteria;
-$criteria->add(new TFilter("nome","like",'pedro%'),TExpression::OR_OPERATOR);
-$criteria->add(new TFilter('nome','like','maria%'),TExpression::OR_OPERATOR);
+$criteria ->add(new TFilter("nome "," like",'pedro%'), TExpression::OR_OPERATOR);
+$criteria ->add(new TFilter('nome ','like', 'maria%'), TExpression::OR_OPERATOR);
+
 echo $criteria->dump();
 echo "<br>\n";
 
-// aqui vemos um exemplo de criério utilizando os operadores "=' e IS NOT
-// neste caso o telefone não pode conter o valor nulo(IS NOT NULL)
-// e o genero deve ser Feminino (sexo = 'F')
+//aqui vemos um exemplo de critério utilizando os operadores '=' e IS NOT
+//neste caso o telefone não pode conter o valor nulo (IS NOT NULL)
+//e o genero deve ser feminino (sexo = 'F')
 
 $criteria = new TCriteria;
-$criteria->add(new TFilter('telefone','IS NOT',NULL));
-$criteria->add(new TFilter('sexo','=','F'));
+$criteria ->add(new TFilter("telefone "," IS NOT ",NULL));
+$criteria ->add(new TFilter("sexo "," = " , ' F '));
+
 echo $criteria->dump();
 echo "<br>\n";
 
-// aqui vemos o uso dos operadores de comparação IN e NOT IN juntamente
-// com conjuntos de strings. nesse caso a UF deve estar entre (RS. SC, PR) e 
-// não deve esar entre (AC,PI)
+//aqui vemos o uso dos operadores de comparação IN e NOT IN juntamente 
+//com conjuntos de strings, nesse caso a UF deve estar entre (RS, SC, PR)
+//não deve estar entre (AC, PI)
 
 $criteria = new TCriteria;
-$criteria->add(new TFilter("UF","IN",array('RS','SC','PR')));
-$criteria->add(new TFilter('UF','NOT IN',array('AC','PI')));
+$criteria ->add(new TFilter("UF","IN",array('RS', 'SC', 'PR')));
+$criteria ->add(new TFilter('UF','NOT IN', array('AC','PI')));
+
 echo $criteria->dump();
 echo "<br>\n";
 
-// neste caso temos o uso de um critério completo
-// o primeiro critério aponta para o sexo ='F'(sexo feminino) e idade > 18
+//neste caso temos o uso de um critério completo
+//o primeiro critério aponta para o sexo = 'F' (feminino) e idade>18
 
 $criteria1 = new TCriteria;
-$criteria1->add(new TFilter('sexo','=','F'));   
-$criteria1->add(new TFilter('idade','>','18') );
+$criteria1 ->add(new TFilter('sexo', '=', 'F'));
+$criteria1 ->add(new TFilter('idade','>', '18'));
 
-// o segundo crit´rio aponta para o sexo ='M'
-// e idade menor de 16 anos
-$criteria2 = new TCriteria;
-$criteria2->add(new TFilter('sexo','=','M') );
-$criteria2->add(new TFilter('idade','<','16') );
+//o segundo critério aponta para o sexo = 'M'
 
-//agora juntamos os dois criterios utilizando o operador lógico OR
-// O resultado deve conter "mulheres maiores de 18 anos" OU "homens menores de 16"
+$criteria2= new TCriteria;
+$criteria2 ->add(new TFilter('sexo', '=', 'M'));
+$criteria2 ->add(new TFilter('idade','<', '16'));
+
+//agora juntamos os dois critérios utilizando o operador lógico OR
+//o resultado deve conter "mulheres maiores que 18 anos" OU "homens menores que 16 anos"
 
 $criteria = new TCriteria;
-$criteria->add($criteria1,TExpression::OR_OPERATOR);
+$criteria ->add($criteria1,TExpression::OR_OPERATOR);
 $criteria->add($criteria2,TExpression::OR_OPERATOR);
+
 echo $criteria->dump();
 echo "<br>\n";
+
+
 
 ?>
